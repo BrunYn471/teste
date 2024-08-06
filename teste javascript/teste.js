2dd7de9df9288a10b3be4fb14449cebe021db323
BrunYn471 <brunobr471@hotmail.com>
2024-08-05 20:19:41 -0400

    Add files via upload

---
+++ teste javascript/teste.js
@@ -0,0 +1,10 @@
+//definindo variavel
+let a = 5;
+let b = 10;
+let soma = a + b;
+
+if (soma > 10) {
+    console.log("A soma é maior que 10");
+} else {
+    console.log("A soma é 10 ou menor");
+}
---
+++ teste javascript/teste2.js
@@ -0,0 +1,5 @@
+function bemvindo(nome, nome2) {
+    return "ola, " + nome + "!\nVai embora, " + nome2 + "!";
+
+}
+bemvindo("Bruno", "putinha");
